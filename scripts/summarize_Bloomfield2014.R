b1665054295d8fb66429c4e6c7a1f40d672d738c
eaperkowski <evan.a.perkowski@ttu.edu>
2025-01-20 23:30:48 -0600

    update workflow such that data cleaning scripts now output .csv file that can be directly merged into mesi data (vs. original copy-past col by col... stupid

--- scripts/summarize_Bloomfield2014.R
+++ scripts/summarize_Bloomfield2014.R
@@ -16,75 +16,188 @@ head(bloomfield_data)
 # How many unique species and growth forms?
 unique(bloomfield_data$Species)
 unique(bloomfield_data$Adult.stature)
+unique(bloomfield_data$Harvest)
 
 # Group by treatment and timepoint, then summarize Bloomfield et al. (2014)
 # for easy input into compiled datasheet
-bloomfield_data %>%
+bloomfield_data_summary <- bloomfield_data %>%
   group_by(Treatment, Months.in) %>%
   summarize(
     
-    n = length(Months.in),
-    
+    Amax_n = sum(!is.na(Amax.a)),
     Amax_mean = mean(Amax.a, na.rm = TRUE),
     Amax_sd = sd(Amax.a, na.rm = TRUE),
-    Amax_se = Amax_sd / sqrt(n),
+    Amax_se = Amax_sd / sqrt(Amax_n),
     
+    Asat_n = sum(!is.na(Asat.a)),
     Asat_mean = mean(Asat.a, na.rm = TRUE),
     Asat_sd = sd(Asat.a, na.rm = TRUE),
-    Asat_se = Asat_sd / sqrt(n),
+    Asat_se = Asat_sd / sqrt(Asat_n),
     
+    Vcmax_n = sum(!is.na(Vcmax.a)),
     Vcmax_mean = mean(Vcmax.a, na.rm = TRUE),
     Vcmax_sd = sd(Vcmax.a, na.rm = TRUE),
-    Vcmax_se = Vcmax_sd / sqrt(n),
+    Vcmax_se = Vcmax_sd / sqrt(Vcmax_n),
     
+    Jmax_n = sum(!is.na(Jmax.a)),
     Jmax_mean = mean(Jmax.a, na.rm = TRUE),
     Jmax_sd = sd(Jmax.a, na.rm = TRUE),
-    Jmax_se = Jmax_sd / sqrt(n),
+    Jmax_se = Jmax_sd / sqrt(Jmax_n),
     
+    TPU_n = sum(!is.na(TPU.a)),
     TPU_mean = mean(TPU.a, na.rm = TRUE),
     TPU_sd = sd(TPU.a, na.rm = TRUE),
-    TPU_se = TPU_sd / sqrt(n),
+    TPU_se = TPU_sd / sqrt(TPU_n),
     
+    Rd_n = sum(!is.na(Rdark.a)),
     Rd_mean = mean(Rdark.a, na.rm = TRUE),
     Rd_sd = sd(Rdark.a, na.rm = TRUE),
-    Rd_se = Rd_sd / sqrt(n),
+    Rd_se = Rd_sd / sqrt(Rd_n),
     
+    cica_n = sum(!is.na(Ci.Ca)),
     cica_mean = mean(Ci.Ca, na.rm = TRUE),
     cica_sd = sd(Ci.Ca, na.rm = TRUE),
-    cica_se = cica_sd / sqrt(n),
+    cica_se = cica_sd / sqrt(cica_n),
     
+    lma_n = sum(!is.na(LMA)),
     lma_mean = mean(LMA, na.rm = TRUE),
     lma_sd = sd(LMA, na.rm = TRUE),
-    lma_se = lma_sd / sqrt(n),
+    lma_se = lma_sd / sqrt(lma_n),
     
+    nmass_n = sum(!is.na(LeafN)),
     nmass_mean = mean(LeafN, na.rm = TRUE),
     nmass_sd = sd(LeafN, na.rm = TRUE),
-    nmass_se = nmass_sd / sqrt(n),
+    nmass_se = nmass_sd / sqrt(nmass_n),
     
+    narea_n = sum(!is.na(N.area)),
     narea_mean = mean(N.area, na.rm = TRUE),
     narea_sd = sd(N.area, na.rm = TRUE),
-    narea_se = narea_sd / sqrt(n),
+    narea_se = narea_sd / sqrt(narea_n),
 
+    pmass_n = sum(!is.na(LeafP)),
     pmass_mean = mean(LeafP, na.rm = TRUE),
     pmass_sd = sd(LeafP, na.rm = TRUE),
-    pmass_se = pmass_sd / sqrt(n),
+    pmass_se = pmass_sd / sqrt(pmass_n),
     
+    parea_n = sum(!is.na(P.area)),
     parea_mean = mean(P.area, na.rm = TRUE),
     parea_sd = sd(P.area, na.rm = TRUE),
-    parea_se = parea_sd / sqrt(n),
+    parea_se = parea_sd / sqrt(parea_n),
     
+    leafnp_n = sum(!is.na(RatioNP)),
     leafnp_mean = mean(RatioNP, na.rm = TRUE),
     leafnp_sd = sd(RatioNP, na.rm = TRUE),
-    leafnp_se = leafnp_sd / sqrt(n),
+    leafnp_se = leafnp_sd / sqrt(leafnp_n),
     
+    pnue_n = sum(!is.na(PNUE)),
     pnue_mean = mean(PNUE, na.rm = TRUE),
     pnue_sd = sd(PNUE, na.rm = TRUE),
-    pnue_se = pnue_sd / sqrt(n),
+    pnue_se = pnue_sd / sqrt(pnue_n),
     
+    ppue_n = sum(!is.na(PPUE)),
     ppue_mean = mean(PPUE, na.rm = TRUE),
     ppue_sd = sd(PPUE, na.rm = TRUE),
-    ppue_se = ppue_sd / sqrt(n)
-    
-  ) %>%
+    ppue_se = ppue_sd / sqrt(ppue_n))
+
+# # Prep for easy merge into compiled datasheet
+bloomfield_data_summary_control <- bloomfield_data_summary %>%
+  ungroup(Treatment) %>%
+  filter(Treatment == "Control") %>%
+  mutate(exp = "bloomfield2014") %>%
+  dplyr::select(-Treatment)
+names(bloomfield_data_summary_control)[2:61] <- str_c(names(bloomfield_data_summary_control)[2:61], "_control")
+
+bloomfield_data_summary_treatment <- bloomfield_data_summary %>%
+  ungroup(Treatment) %>%
+  filter(Treatment != "Control") %>%
+  mutate(exp = "bloomfield2014")
+names(bloomfield_data_summary_treatment)[3:62] <- str_c(names(bloomfield_data_summary_treatment)[3:62], "_trt")
+
+
+test <- bloomfield_data_summary_control %>%
+  full_join(bloomfield_data_summary_treatment, by = c("exp", "Months.in")) %>%
+  dplyr::select(exp, Treatment, Months.in, 
+                
+                Amax_mean_control, Amax_mean_trt, 
+                Amax_sd_control, Amax_sd_trt, 
+                Amax_se_control, Amax_se_trt, 
+                Amax_n_control, Amax_n_trt,
+              
+                Asat_mean_control, Asat_mean_trt, 
+                Asat_sd_control, Asat_sd_trt, 
+                Asat_se_control, Asat_se_trt, 
+                Asat_n_control, Asat_n_trt,
+                
+                Vcmax_mean_control, Vcmax_mean_trt, 
+                Vcmax_sd_control, Vcmax_sd_trt, 
+                Vcmax_se_control, Vcmax_se_trt, 
+                Vcmax_n_control, Vcmax_n_trt,
+                
+                Jmax_mean_control, Jmax_mean_trt, 
+                Jmax_sd_control, Jmax_sd_trt, 
+                Jmax_se_control, Jmax_se_trt, 
+                Jmax_n_control, Jmax_n_trt,
+                
+                TPU_mean_control, TPU_mean_trt, 
+                TPU_sd_control, TPU_sd_trt, 
+                TPU_se_control, TPU_se_trt, 
+                TPU_n_control, TPU_n_trt,
+                
+                Rd_mean_control, Rd_mean_trt, 
+                Rd_sd_control, Rd_sd_trt, 
+                Rd_se_control, Rd_se_trt, 
+                Rd_n_control, Rd_n_trt,
+                
+                cica_mean_control, cica_mean_trt, 
+                cica_sd_control, cica_sd_trt, 
+                cica_se_control, cica_se_trt,
+                cica_n_control, cica_n_trt,
+                
+                lma_mean_control, lma_mean_trt, 
+                lma_sd_control, lma_sd_trt, 
+                lma_se_control, lma_se_trt, 
+                lma_n_control, lma_n_trt,
+                
+                nmass_mean_control, nmass_mean_trt, 
+                nmass_sd_control, nmass_sd_trt, 
+                nmass_se_control, nmass_se_trt, 
+                nmass_n_control, nmass_n_trt,
+                
+                narea_mean_control, narea_mean_trt, 
+                narea_sd_control, narea_sd_trt, 
+                narea_se_control, narea_se_trt, 
+                narea_n_control, narea_n_trt,
+                
+                pmass_mean_control, pmass_mean_trt, 
+                pmass_sd_control, pmass_sd_trt, 
+                pmass_se_control, pmass_se_trt, 
+                pmass_n_control, pmass_n_trt,
+                
+                parea_mean_control, parea_mean_trt, 
+                parea_sd_control, parea_sd_trt, 
+                parea_se_control, parea_se_trt, 
+                parea_n_control, parea_n_trt,
+                
+                leafnp_mean_control, leafnp_mean_trt, 
+                leafnp_sd_control, leafnp_sd_trt, 
+                leafnp_se_control, leafnp_se_trt, 
+                leafnp_n_control, leafnp_n_trt,
+                
+                pnue_mean_control, pnue_mean_trt, 
+                pnue_sd_control, pnue_sd_trt, 
+                pnue_se_control, pnue_se_trt, 
+                pnue_n_control, pnue_n_trt,
+                
+                ppue_mean_control, ppue_mean_trt, 
+                ppue_sd_control, ppue_sd_trt, 
+                ppue_se_control, ppue_se_trt, 
+                ppue_n_control, ppue_n_trt)  %>%
+  pivot_longer(cols = Amax_mean_control:ppue_n_trt,
+               names_to = c("trait", "stat", "trt"), 
+               names_sep = "_",
+               values_to = "value") %>%
+  pivot_wider(names_from = stat:trt,
+              values_from = value) %>%
+  arrange(trait) %>%
   write.csv("../data_summaries/Bloomfield2014_FPB_summarized.csv", row.names = F)
   
--- scripts/summarize_Fan2024.R
+++ scripts/summarize_Fan2024.R
@@ -1,90 +1,216 @@
 # analyze Fan et al. (2024) data
 
-
-
 # Libraries
 library(tidyverse)
 
 # Load Mo et al. (2019) data
-fan_data <- read.csv("../raw_data/Fan_2024_pFraction_data.csv")
+fan_data <- read.csv("../raw_data/Fan_2024_pFraction_data.csv") %>%
+  slice(1:122) %>%
+  mutate(leaf_np = nmass / pmass)
 
 # Group by and summarize Fan et al. (2024) dataset for easy input into 
 # compiled datasheet
 head(fan_data)
 
-fan_data %>%
+fan_data_summary <- fan_data %>%
 
   group_by(treatment) %>%
-  summarize(n = length(lma),
-            
+  summarize(lma_n = sum(!is.na(lma)),
             lma_mean = mean(lma, na.rm = TRUE),
             lma_sd = sd(lma, na.rm =TRUE),
-            lma_se = lma_sd / sqrt(n),
+            lma_se = lma_sd / sqrt(lma_n),
             
+            spad_n = sum(!is.na(spad)),
             spad_mean = mean(spad, na.rm = TRUE),
             spad_sd = sd(spad, na.rm =TRUE),
-            spad_se = spad_sd / sqrt(n),
+            spad_se = spad_sd / sqrt(spad_n),
             
+            nmass_n = sum(!is.na(nmass)),
             nmass_mean = mean(nmass, na.rm = TRUE),
             nmass_sd = sd(nmass, na.rm =TRUE),
-            nmass_se = nmass_sd / sqrt(n),
+            nmass_se = nmass_sd / sqrt(nmass_n),
             
+            narea_n = sum(!is.na(narea)),
             narea_mean = mean(narea, na.rm = TRUE),
             narea_sd = sd(narea, na.rm =TRUE),
-            narea_se = narea_sd / sqrt(n),
+            narea_se = narea_sd / sqrt(narea_n),
             
+            pmass_n = sum(!is.na(parea)),
             pmass_mean = mean(pmass, na.rm = TRUE),
             pmass_sd = sd(pmass, na.rm =TRUE),
-            pmass_se = pmass_sd / sqrt(n),
+            pmass_se = pmass_sd / sqrt(pmass_n),
             
+            parea_n = sum(!is.na(parea)),
             parea_mean = mean(parea, na.rm = TRUE),
             parea_sd = sd(parea, na.rm =TRUE),
-            parea_se = parea_sd / sqrt(n),
+            parea_se = parea_sd / sqrt(parea_n),
             
+            leafnp_n = sum(!is.na(leaf_np)),
+            leafnp_mean = mean(leaf_np, na.rm = TRUE),
+            leafnp_sd = sd(leaf_np, na.rm =TRUE),
+            leafnp_se = leafnp_sd / sqrt(leafnp_n),
+            
+            rgr_n = sum(!is.na(rgr)),
             rgr_mean = mean(rgr, na.rm = TRUE),
             rgr_sd = sd(rgr, na.rm =TRUE),
-            rgr_se = rgr_sd / sqrt(n),
+            rgr_se = rgr_sd / sqrt(rgr_n),
             
+            amax_n = sum(!is.na(Amax)),
             amax_mean = mean(Amax, na.rm = TRUE),
             amax_sd = sd(Amax, na.rm =TRUE),
-            amax_se = amax_sd / sqrt(n),
+            amax_se = amax_sd / sqrt(amax_n),
             
+            vcmax_n = sum(!is.na(Vcmax)),
             vcmax_mean = mean(Vcmax, na.rm = TRUE),
             vcmax_sd = sd(Vcmax, na.rm =TRUE),
-            vcmax_se = vcmax_sd / sqrt(n),
+            vcmax_se = vcmax_sd / sqrt(vcmax_n),
             
+            jmax_n = sum(!is.na(Jmax)),
             jmax_mean = mean(Jmax, na.rm = TRUE),
             jmax_sd = sd(Jmax, na.rm =TRUE),
-            jmax_se = jmax_sd / sqrt(n),
+            jmax_se = jmax_sd / sqrt(jmax_n),
             
-            ppue_mean = mean(PPUE, na.rm = TRUE),
-            ppue_sd = sd(PPUE, na.rm =TRUE),
-            ppue_se = ppue_sd / sqrt(n),
-            
+            pnue_n = sum(!is.na(PNUE)),
             pnue_mean = mean(PNUE, na.rm = TRUE),
             pnue_sd = sd(PNUE, na.rm =TRUE),
-            pnue_se = pnue_sd / sqrt(n),
+            pnue_se = pnue_sd / sqrt(pnue_n),
             
+            ppue_n = sum(!is.na(PPUE)),
+            ppue_mean = mean(PPUE, na.rm = TRUE),
+            ppue_sd = sd(PPUE, na.rm =TRUE),
+            ppue_se = ppue_sd / sqrt(ppue_n),
+            
+            pi_n = sum(!is.na(Pi)),
             pi_mean = mean(Pi, na.rm = TRUE),
             pi_sd = sd(Pi, na.rm =TRUE),
-            pi_se = pi_sd / sqrt(n),
+            pi_se = pi_sd / sqrt(pi_n),
             
+            esterp_n = sum(!is.na(EsterP)),
             esterp_mean = mean(EsterP, na.rm = TRUE),
             esterp_sd = sd(EsterP, na.rm =TRUE),
-            esterp_se = esterp_sd / sqrt(n),
+            esterp_se = esterp_sd / sqrt(esterp_n),
             
+            nucleicp_n = sum(!is.na(Nucleic.P)),
             nucleicp_mean = mean(Nucleic.P, na.rm = TRUE),
             nucleicp_sd = sd(Nucleic.P, na.rm =TRUE),
-            nucleicp_se = nucleicp_sd / sqrt(n),
+            nucleicp_se = nucleicp_sd / sqrt(nucleicp_n),
             
+            lipidp_n = sum(!is.na(Lipid.P)),
             lipidp_mean = mean(Lipid.P, na.rm = TRUE),
             lipidp_sd = sd(Lipid.P, na.rm =TRUE),
-            lipidp_se = lipidp_sd / sqrt(n),
+            lipidp_se = lipidp_sd / sqrt(lipidp_n),
             
+            residualp_n = sum(!is.na(Residual.P)),
             residualp_mean = mean(Residual.P, na.rm = TRUE),
             residualp_sd = sd(Residual.P, na.rm =TRUE),
-            residualp_se = residualp_sd / sqrt(n)
-            
-            ) %>%
-  slice(-1) %>%
+            residualp_se = residualp_sd / sqrt(residualp_n))
+
+
+# Prep for easy merge into compiled datasheet
+fan_data_summary_control <- fan_data_summary %>%
+  filter(treatment == "CK") %>%
+  mutate(exp = "Fan2024") %>%
+  select(-treatment)
+names(fan_data_summary_control)[1:72] <- str_c(names(fan_data_summary_control)[1:72], "_control")
+
+fan_data_summary_treatment <- fan_data_summary %>%
+  filter(treatment != "CK") %>%
+  mutate(exp = "Fan2024")
+names(fan_data_summary_treatment)[2:73] <- str_c(names(fan_data_summary_treatment)[2:73], "_trt")
+
+# Format into easy merge into compiled datasheet, write to .csv
+fan_data_summary_control %>%
+  full_join(fan_data_summary_treatment, by = "exp") %>%
+  dplyr::select(exp, treatment,
+                
+                lma_mean_control, lma_mean_trt, 
+                lma_sd_control, lma_sd_trt, 
+                lma_se_control, lma_se_trt, 
+                lma_n_control, lma_n_trt,
+                
+                nmass_mean_control, nmass_mean_trt, 
+                nmass_sd_control, nmass_sd_trt, 
+                nmass_se_control, nmass_se_trt, 
+                nmass_n_control, nmass_n_trt,
+                
+                narea_mean_control, narea_mean_trt, 
+                narea_sd_control, narea_sd_trt, 
+                narea_se_control, narea_se_trt, 
+                narea_n_control, narea_n_trt,
+                
+                pmass_mean_control, pmass_mean_trt, 
+                pmass_sd_control, pmass_sd_trt, 
+                pmass_se_control, pmass_se_trt, 
+                pmass_n_control, pmass_n_trt,
+                
+                parea_mean_control, parea_mean_trt, 
+                parea_sd_control, parea_sd_trt, 
+                parea_se_control, parea_se_trt, 
+                parea_n_control, parea_n_trt,
+                
+                leafnp_mean_control, leafnp_mean_trt, 
+                leafnp_sd_control, leafnp_sd_trt, 
+                leafnp_se_control, leafnp_se_trt, 
+                leafnp_n_control, leafnp_n_trt,
+                
+                rgr_mean_control, rgr_mean_trt, 
+                rgr_sd_control, rgr_sd_trt, 
+                rgr_se_control, rgr_se_trt, 
+                rgr_n_control, rgr_n_trt,
+                
+                amax_mean_control, amax_mean_trt, 
+                amax_sd_control, amax_sd_trt, 
+                amax_se_control, amax_se_trt, 
+                amax_n_control, amax_n_trt,
+                
+                vcmax_mean_control, vcmax_mean_trt, 
+                vcmax_sd_control, vcmax_sd_trt, 
+                vcmax_se_control, vcmax_se_trt, 
+                vcmax_n_control, vcmax_n_trt,
+                
+                jmax_mean_control, jmax_mean_trt, 
+                jmax_sd_control, jmax_sd_trt, 
+                jmax_se_control, jmax_se_trt, 
+                jmax_n_control, jmax_n_trt,
+                
+                pnue_mean_control, pnue_mean_trt, 
+                pnue_sd_control, pnue_sd_trt, 
+                pnue_se_control, pnue_se_trt, 
+                pnue_n_control, pnue_n_trt,
+                
+                ppue_mean_control, ppue_mean_trt, 
+                ppue_sd_control, ppue_sd_trt, 
+                ppue_se_control, ppue_se_trt, 
+                ppue_n_control, ppue_n_trt,
+                
+                pi_mean_control, pi_mean_trt, 
+                pi_sd_control, pi_sd_trt, 
+                pi_se_control, pi_se_trt, 
+                pi_n_control, pi_n_trt,
+                
+                esterp_mean_control, esterp_mean_trt, 
+                esterp_sd_control, esterp_sd_trt, 
+                esterp_se_control, esterp_se_trt, 
+                esterp_n_control, esterp_n_trt,
+                
+                nucleicp_mean_control, nucleicp_mean_trt, 
+                nucleicp_sd_control, nucleicp_sd_trt, 
+                nucleicp_se_control, nucleicp_se_trt, 
+                nucleicp_n_control, nucleicp_n_trt,
+                
+                lipidp_mean_control, lipidp_mean_trt, 
+                lipidp_sd_control, lipidp_sd_trt, 
+                lipidp_se_control, lipidp_se_trt, 
+                lipidp_n_control, lipidp_n_trt,
+                
+                residualp_mean_control, residualp_mean_trt, 
+                residualp_sd_control, residualp_sd_trt, 
+                residualp_se_control, residualp_se_trt, 
+                residualp_n_control, residualp_n_trt) %>%
+  pivot_longer(cols = lma_mean_control:residualp_n_trt,
+               names_to = c("trait", "stat", "trt"), 
+               names_sep = "_",
+               values_to = "value") %>%
+  pivot_wider(names_from = stat:trt,
+              values_from = value) %>%
   write.csv("../data_summaries/Fan2024_pFraction_data_summary.csv", row.names = F)
