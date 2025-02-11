5a3d27a757e3ad8957cbc194a3a886cb5f777383
eaperkowski <evan.a.perkowski@ttu.edu>
2025-02-10 21:30:16 -0600

    add digitized plot data for Hayes 2022 and Black 2000

--- scripts/digitize_plots.R
+++ scripts/digitize_plots.R
@@ -6,8 +6,13 @@ chen2024 <- metaDigitise(dir = "../plots_to_digitize/Chen_2024_jxb/")
 ## write.csv(chen2024, "../raw_data/Chen_2024_jxb_digitized.csv", row.names = F)
 
 # Ye et al. 2022
-
 ye2022 <- metaDigitise(dir = "../plots_to_digitize/Ye_2022_jfr/")
 ## write.csv(ye2022, "../raw_data/Ye_2022_jfr_digitized.csv", row.names = F)
 
+# Black et al. 2000
+black2000 <- metaDigitise(dir = "../plots_to_digitize/Black_2000_pce/")
+# write.csv(black2000, "../raw_data/Black_2000_pce_digitized.csv", row.names = F)
 
+# Hayes et al. 2022
+hayes2022 <- metaDigitise(dir = "../plots_to_digitize/Hayes_2022_aob/")
+# write.csv(hayes2022, "../raw_data/Hayes_2022_aob.csv", row.names = F)
