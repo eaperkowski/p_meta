1a228b83b3f9d3c70900fa8529baf6f9f3c25f53
eaperkowski <evan.a.perkowski@ttu.edu>
2025-04-10 12:58:27 -0500

    finish cleaning effect sizes. ready to go for meta analysis!

--- scripts/NPmeta_analysis_effectSizes.R
+++ scripts/NPmeta_analysis_effectSizes.R
@@ -4,6 +4,10 @@
 # measurements are aggregated at the experiment level and leaf-level
 # measurements are aggregated at the species level.
 
+###############################################################################
+# Load libraries, data, and helper functions
+###############################################################################
+
 # Libraries
 library(tidyverse)
 library(metafor)
@@ -22,9 +26,10 @@ eap <- read.csv("../data/eap_main.csv")
 # effect sizes)
 source("../helper_fxns/calc_intxn_effSize_meta.R")
 
-# Merge data sources into single data frame, then convert dataset to
-# wide format to make for easy determination of individual, main, and
-# interaction effect sizes
+###############################################################################
+# Merge data sources, convert to wide format for easy effect size calculation
+###############################################################################
+# Merge data sources into single data frame
 full_df <- mesi %>% full_join(nutnet) %>% full_join(eap) %>%
   replace_with_na_all(~.x == "<NA>") %>%
   dplyr::select(-doi, -x_units, -se_c, -se_t) %>%
@@ -32,20 +37,28 @@ full_df <- mesi %>% full_join(nutnet) %>% full_join(eap) %>%
                        "n", ifelse(npk == "_010",
                                    "p", ifelse(npk == "_110", "np", NA))))
 
-head(full_df)
+# Create experiment metadata df, prep for merge after effect size calculation
+experiment_summary <- full_df %>%
+  dplyr::select(citation, exp:experiment_type) %>%
+  distinct(citation, exp, .keep_all = TRUE)
+
+
+# Create df that only includes control trt, prep for merge to long format
 full_df_control <- full_df %>%
-  mutate(unique_id = ifelse(is.na(species), 
-                            str_c(citation, "XXX", site, "XXX", response, "XXX", "none"),
-                            str_c(citation, "XXX", site, "XXX", response, "XXX", species))) %>%
+  mutate(unique_id = ifelse(
+    is.na(species),
+    str_c(citation, "XXX", exp, "XXX", response, "XXX", "none"),
+    str_c(citation, "XXX", exp, "XXX", response, "XXX", species))) %>%
   group_by(unique_id) %>%
   summarize(x_c = mean(x_c),
             sd_c = mean(sd_c),
             rep_c = mean(rep_c))
 
+# Convert df into long format to make for easy effect size calculation
 full_df_trt_long <- full_df %>% 
   mutate(unique_id = ifelse(is.na(species), 
-                            str_c(citation, "XXX", site, "XXX", response, "XXX", "none"),
-                            str_c(citation, "XXX", site, "XXX", response, "XXX", species))) %>%
+                            str_c(citation, "XXX", exp, "XXX", response, "XXX", "none"),
+                            str_c(citation, "XXX", exp, "XXX", response, "XXX", species))) %>%
   dplyr::select(unique_id, fert, x_t, sd_t, rep_t) %>%
   pivot_wider(names_from = fert, values_from = c(x_t, sd_t, rep_t)) %>%
   full_join(full_df_control) %>%
@@ -54,25 +67,50 @@ full_df_trt_long <- full_df %>%
                 x_n = x_t_n, sd_n = sd_t_n, rep_n = rep_t_n,
                 x_p = x_t_p, sd_p = sd_t_p, rep_p = rep_t_p,
                 x_np = x_t_np, sd_np = sd_t_np, rep_np = rep_t_np)
- 
-calc_intxn_effSize_meta(x_a = full_df_trt_long$x_n, 
-                        s_a = full_df_trt_long$sd_n, 
-                        n_a = full_df_trt_long$rep_n,
-                        
-                        x_b = full_df_trt_long$x_p, 
-                        s_b = full_df_trt_long$sd_p, 
-                        n_b = full_df_trt_long$rep_p,
-                        
-                        x_c = full_df_trt_long$x_c, 
-                        s_c = full_df_trt_long$sd_c, 
-                        n_c = full_df_trt_long$rep_c,
-                        
-                        x_ab = full_df_trt_long$x_np, 
-                        s_ab = full_df_trt_long$sd_np, 
-                        n_ab = full_df_trt_long$rep_np)
 
-  
+###############################################################################
+# Calculate individual, main, and interaction effect sizes
+###############################################################################
+effect_sizes <- data.frame(
+  unique_id = full_df_trt_long$unique_id,
+  calc_intxn_effSize_meta(x_a = full_df_trt_long$x_n, 
+                          s_a = full_df_trt_long$sd_n, 
+                          n_a = full_df_trt_long$rep_n,
+                          
+                          x_b = full_df_trt_long$x_p, 
+                          s_b = full_df_trt_long$sd_p, 
+                          n_b = full_df_trt_long$rep_p,
+                          
+                          x_c = full_df_trt_long$x_c, 
+                          s_c = full_df_trt_long$sd_c, 
+                          n_c = full_df_trt_long$rep_c,
+                          
+                          x_ab = full_df_trt_long$x_np, 
+                          s_ab = full_df_trt_long$sd_np, 
+                          n_ab = full_df_trt_long$rep_np)) %>%
+  separate(unique_id, 
+           into = c("citation", "exp", "response", "species"), 
+           sep = "XXX", remove = FALSE) %>%
+  full_join(experiment_summary, by = c("citation", "exp")) %>%
+  dplyr::select(citation, exp, latitude:experiment_type, 
+                species, response,
+                
+                # Individual effects
+                gN = g_a, vN = v_a, wN = w_a,
+                gP = g_b, vP = v_b, wP = w_b,
+                gNP = g_ab, vNP = v_ab, wNP = w_ab,
+                
+                # Main effects
+                dN = dA, dvN = v_a_main, dwN = w_a_main,
+                dP = dB, dvP = v_b_main, dwP = w_b_main,
+                
+                # Interaction effects
+                dNP = dAB, dvNP = v_ab_int, dwNP = w_ab_int) %>%
+  replace_with_na_all(~.x == "none")
 
+###############################################################################
+# From effect size, variance, and weight, calculate weighted means
+###############################################################################
 
 
 
