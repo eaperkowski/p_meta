f05d31cd15e31e083df015b2464bd309942fb114
eaperkowski <evan.a.perkowski@ttu.edu>
2026-02-03 16:16:28 -0600

    add final analyses

--- scripts/CNPmeta_analysis.R
+++ scripts/CNPmeta_analysis.R
@@ -15,14 +15,8 @@ library(orchaRd)
 
 # Read compiled dataset
 full_df <- read.csv("../data/CNP_data_compiled.csv") %>%
-  replace_with_na_all(~.x == "<NA>") #%>%
-  #mutate(pft = str_c(photo_path, n_fixer, myc_assoc, sep = "_"),
-  #       myc_nas = ifelse(myc_assoc == "EcM" | myc_assoc == "EcM-AM" |
-  #                          myc_assoc == "ErM", "mining",
-  #                        ifelse(myc_assoc == "NM-AM" |
-  #                                 myc_assoc == "AM" | myc_assoc == "NM",
-  #                               "scavenging",
-  #                               NA)))
+  replace_with_na_all(~.x == "<NA>") %>%
+  filter(treatment == "fnp")
 
 # Create experiment metadata summary
 experiment_summary <- full_df %>%
@@ -35,6 +29,7 @@ species_summary <- full_df %>%
            photo_path, n_fixer, myc_assoc, .keep_all = TRUE) %>%
   dplyr::select(species:myc_assoc) %>%
   slice(-1)
+
 
 # How many experiments?
 unique(experiment_summary$citation)
@@ -73,26 +68,25 @@ unique(nfert_only$response)
 # Which experiments are included?
 unique(nfert_only$citation)
 
+unique(nfert_only$response)
+
 # Select variables
-use_response_n <- c("agb_n", "agb_p", "agb", "anpp", "agb_n_mass",
-                    "agb_p_mass", "leaf_n_mass", "leaf_p_mass", "anet",
-                    "total_biomass", "bgb", "rootshoot", "bnpp", "lma", "sla", "rmf",
-                    "gsw", "amax", "leaf_wue", "fine_root_biomass", "leaf_n_area",
-                    "leaf_p_area", "asat", "vcmax", "jmax", "jmax_vcmax", "leaf_pnue", "leaf_np",
-                    "leaf_ppue", "leaf_structural_p", "leaf_metabolic_p",
-                    "leaf_nucleic_p", "leaf_residual_p", "leaf_pi")
+use_response_n <- c("anpp_n", "anpp_p", "anpp", "leaf_n_mass", "leaf_p_mass", "anet",
+                    "tbio_gm2", "bnpp", "rootshoot", "bgb", "lma",
+                    "lai", "rmf", "tla", "gsw", "agb", "leaf_np", "total_biomass",
+                    "asat", "leaf_wue", "leaf_n_area", "leaf_p_area", "vcmax", 
+                    "jmax", "jmax_vcmax", "rd", "leaf_pnue", "leaf_ppue",
+                    "leaf_structural_p", "leaf_metabolic_p", "leaf_nucleic_p",
+                    "leaf_residual_p", "amax", "leaf_pi", "leaf_nre", "leaf_pre", 
+                    "fine_root_biomass")
 
 nfert_responses <- nfert_only %>%
   filter(response %in% use_response_n) %>%
   mutate(myvar = response) %>%
   mutate(myvar = ifelse(myvar %in% c("amax", "anet", "asat"),
                         "asat", myvar),
-         myvar = ifelse(myvar %in% c("agb", "anpp"),
-                        "agb", myvar),
-         myvar = ifelse(myvar %in% c("bgb", "fine_root_biomass", "bnpp"),
-                        "bgb", myvar),
-         myvar = ifelse(myvar %in% c("total_leaf_area", "tla"),
-                        "tla", myvar))
+         myvar = ifelse(myvar %in% c("bgb", "fine_root_biomass"),
+                        "bgb", myvar))
 
 use_vars_n <- unique(nfert_responses$myvar)
 
@@ -119,16 +113,20 @@ out_n <- purrr::map(as.list(use_vars_n),
 names(out_n) <- use_vars_n
 
 df_box_n_k <- data.frame(var = use_vars_n,
-                          k = c("(18)", "(12)", "(125)", "(30)", "(23)", "(139)",
-                                "(133)", "(89)", "(42)", "(63)", "(40)",
-                                NA, "(37)", "(43)", "(118)", "(113)", "(40)", 
-                                "(87)", "(82)", "(42)", "(40)", "(32)", "(61)", "(62)", 
-                                "(20)", "(21)", "(21)", "(20)", "(5)"),
-                         sig.level = c("***", "***", "***", "***", "***", "***",
-                                       "*  ", ".  ", "   ", "   ", "***", 
-                                       NA, "***", "   ", "** ", "*  ", "   ", 
-                                       "** ", "   ", "   ", "   ", ".  ", "   ", ".  ", 
-                                       "   ", "   ", "   ", "   ", "   "))
+                          k = c("(15)", "(12)", "(114)", "(147)", "(141)",
+                                "(95)", "(33)", "(54)", "(47)", "(19)",
+                                "(116)", "(5)", "(37)", "(9)", "(48)",
+                                "(23)", "(123)", "(17)", "(40)", "(87)",
+                                "(82)", "(42)", "(40)", "(32)", "(32)",
+                                "(65)", "(66)", "(23)", "(24)", "(24)",
+                                "(23)", "(5)", "(5)", "(5)"),
+                         sig.level = c("***", "***", "***", "***", "** ",
+                                       "** ", "   ", "   ", "***", "   ",
+                                       "*   ", "   ", "***", "***", "   ",
+                                       "*   ", "***", "   ", "   ", "** ",
+                                       "   ", "   ", "   ", "   .", "***",
+                                       "   ", "** ", "   ", "   ", "   ",
+                                       "   ", "   ", "   ", "   "))
 df_box_n_k$k_sig <- str_c(df_box_n_k$k, df_box_n_k$sig.level)
 
 df_box_n <- purrr::map_dfr(out_n, "df_box") |> 
@@ -152,25 +150,22 @@ head(pfert_only)
 unique(pfert_only$response)
 
 # Select variables
-use_response_p <- c("agb_n", "agb_p", "agb", "anpp", "agb_n_mass",
-                    "agb_p_mass", "leaf_n_mass", "leaf_p_mass", "anet",
-                    "total_biomass", "bgb", "rootshoot", "bnpp", "lma", "sla", "rmf",
-                    "gsw", "amax", "leaf_wue", "fine_root_biomass", "leaf_n_area",
-                    "leaf_p_area", "asat", "vcmax", "jmax", "jmax_vcmax", "leaf_pnue", "leaf_np",
-                    "leaf_ppue", "leaf_structural_p", "leaf_metabolic_p",
-                    "leaf_nucleic_p", "leaf_residual_p", "leaf_pi")
+use_response_p <- c("anpp_n", "anpp_p", "anpp", "leaf_n_mass", "leaf_p_mass", "anet",
+                    "tbio_gm2", "bnpp", "rootshoot", "bgb", "lma",
+                    "lai", "rmf", "tla", "gsw", "agb", "leaf_np", "total_biomass",
+                    "asat", "leaf_wue", "leaf_n_area", "leaf_p_area", "vcmax", 
+                    "jmax", "jmax_vcmax", "rd", "leaf_pnue", "leaf_ppue",
+                    "leaf_structural_p", "leaf_metabolic_p", "leaf_nucleic_p",
+                    "leaf_residual_p", "amax", "leaf_pi", "leaf_nre", "leaf_pre", 
+                    "fine_root_biomass")
 
 pfert_responses <- pfert_only %>%
   filter(response %in% use_response_p) %>%
   mutate(myvar = response) %>%
   mutate(myvar = ifelse(myvar %in% c("amax", "anet", "asat"),
                         "asat", myvar),
-         myvar = ifelse(myvar %in% c("agb", "anpp"),
-                        "agb", myvar),
-         myvar = ifelse(myvar %in% c("bgb", "fine_root_biomass", "bnpp"),
-                        "bgb", myvar),
-         myvar = ifelse(myvar %in% c("total_leaf_area", "tla"),
-                        "tla", myvar))
+         myvar = ifelse(myvar %in% c("bgb", "fine_root_biomass"),
+                        "bgb", myvar))
 
 use_vars_p <- unique(pfert_responses$myvar)
 
@@ -182,9 +177,7 @@ pfert_lnRR <- pfert_responses %>%
          data = ., 
          append = TRUE, 
          var.names = c("logr", "logr_var")) %>%
-  mutate(logr_se = sqrt(logr_var) / sqrt(rep_t)) %>%
-  mutate(logr = ifelse(myvar == "sla", logr * -1, logr), 
-         myvar = ifelse(myvar == "sla", "lma", myvar))
+  mutate(logr_se = sqrt(logr_var) / sqrt(rep_t))
 
 # Determine lnRR across multiple experiments. Takes into account
 # experiment variance and uses experiment identity as grouping
@@ -195,16 +188,18 @@ out_p <- purrr::map(as.list(use_vars_p),
 names(out_p) <- use_vars_p
 
 df_box_p_k <- data.frame(var = use_vars_p,
-                         k = c("(18)", "(12)", "(125)", "(30)", "(23)", "(139)",
-                               "(133)", "(89)", "(42)", "(63)", "(40)", NA,
-                               "(37)", "(43)", "(118)", "(113)", "(40)", "(87)", 
-                               "(82)", "(42)", "(40)", "(32)", "(61)", "(62)", "(20)",
-                               "(21)", "(21)", "(20)", "(5)"),
-                         sig.level = c("   ", "***", "***", "***", "***", "   ",
-                                       "***", "   ", "*  ", "   ", "*  ", NA,
-                                       "   ", "   ", "***", "   ", "   ", "   ", 
-                                       "***", "   ", "*  ", "   ", "   ", "   ", "   ",
-                                       "*  ", "   ", "   ", "** "))
+                         k = c("(15)", "(12)", "(114)", "(147)", "(141)", "(95)",
+                               "(33)", "(54)", "(47)", "(19)", "(116)",
+                               "(5)", "(37)", "(9)", "(48)", "(23)", "(123)", 
+                               "(17)", "(40)", "(87)", "(82)", "(42)", "(40)", 
+                               "(32)", "(32)", "(65)", "(66)", "(23)", "(24)",
+                               "(24)", "(23)", "(5)", "(5)", "(5)"),
+                         sig.level = c("   ", "***", "***", "   ", "***", "   ",
+                                       ".  ", "   ", "*  ", ".  ", "   ", 
+                                       ".  ", ".  ", "** ", "*  ", "*  ", "***",
+                                       "** ", "   ", "   ", "***", "   ", "*  ", 
+                                       "   ", "*  ", ".  ", ".  ", "   ", "** ",
+                                       "   ", "*  ", "** ", "   ", "   "))
 df_box_p_k$k_sig <- str_c(df_box_p_k$k, df_box_p_k$sig.level)
 
 df_box_p <- purrr::map_dfr(out_p, "df_box") |> 
@@ -228,29 +223,23 @@ head(npfert_only)
 unique(npfert_only$response)
 
 # Select variables
-use_response_np <- c("agb_n", "agb_p", "agb", "anpp", "agb_n_mass",
-                    "agb_p_mass", "leaf_n_mass", "leaf_p_mass", "anet",
-                    "total_biomass", "bgb", "rootshoot", "bnpp", "lma", "sla", "rmf",
-                    "gsw", "amax", "leaf_wue", "fine_root_biomass", "leaf_n_area",
-                    "leaf_p_area", "asat", "vcmax", "jmax", "jmax_vcmax", "leaf_pnue", "leaf_np",
-                    "leaf_ppue", "leaf_structural_p", "leaf_metabolic_p",
-                    "leaf_nucleic_p", "leaf_residual_p", "leaf_pi")
+use_response_np <- c("anpp_n", "anpp_p", "anpp", "leaf_n_mass", "leaf_p_mass", "anet",
+                    "tbio_gm2", "bnpp", "rootshoot", "bgb", "lma",
+                    "lai", "rmf", "tla", "gsw", "agb", "leaf_np", "total_biomass",
+                    "asat", "leaf_wue", "leaf_n_area", "leaf_p_area", "vcmax", 
+                    "jmax", "jmax_vcmax", "rd", "leaf_pnue", "leaf_ppue",
+                    "leaf_structural_p", "leaf_metabolic_p", "leaf_nucleic_p",
+                    "leaf_residual_p", "amax", "leaf_pi", "leaf_nre", "leaf_pre", 
+                    "fine_root_biomass")
 
 npfert_responses <- npfert_only %>%
   filter(response %in% use_response_np) %>%
   mutate(myvar = response) %>%
   mutate(myvar = ifelse(myvar %in% c("amax", "anet", "asat"),
                         "asat", myvar),
-         myvar = ifelse(myvar %in% c("agb", "anpp"),
-                        "agb", myvar),
-         myvar = ifelse(myvar %in% c("bgb", "fine_root_biomass", "bnpp"),
-                        "bgb", myvar),
-         myvar = ifelse(myvar %in% c("total_leaf_area", "tla"),
-                        "tla", myvar)) %>%
-  filter(myvar != "r_eco" & myvar != "nee" & 
-           myvar != "spad" & myvar != "tpu" &
-           myvar != "rd" & myvar != "stom_lim" &
-           myvar != "anet_mass")
+         myvar = ifelse(myvar %in% c("bgb", "fine_root_biomass"),
+                        "bgb", myvar))
+
 
 use_vars_np <- unique(npfert_responses$myvar)
 
@@ -262,9 +251,7 @@ npfert_lnRR <- npfert_responses %>%
          data = ., 
          append = TRUE, 
          var.names = c("logr", "logr_var")) %>%
-  mutate(logr_se = sqrt(logr_var) / sqrt(rep_t)) %>%
-  mutate(logr = ifelse(myvar == "sla", logr * -1, logr), 
-         myvar = ifelse(myvar == "sla", "lma", myvar))
+  mutate(logr_se = sqrt(logr_var) / sqrt(rep_t))
 
 # Determine lnRR across multiple experiments. Takes into account
 # experiment variance and uses experiment identity as grouping
@@ -275,16 +262,19 @@ out_np <- purrr::map(as.list(use_vars_np),
 names(out_np) <- use_vars_np
 
 df_box_np_k <- data.frame(var = use_vars_np,
-                          k = c("(18)", "(12)", "(125)", "(30)", "(23)", "(139)",
-                                "(133)", "(89)", "(42)", "(63)", "(40)", NA,
-                                "(37)", "(43)", "(118)", "(113)", "(40)", "(87)", 
-                                "(82)", "(42)", "(40)", "(32)", "(61)", "(62)", "(20)",
-                                "(21)", "(21)", "(20)", "(5)"),
-                          sig.level = c("***", "***", "***", "** ", "*  ", "***",
-                                        "***", "** ", "***", "   ", "***", NA,
-                                        "** ", "*  ", "***", "** ", "*  ", "***", 
-                                        "***", ".  ", "***", "***", "   ", "   ", "   ",
-                                        "*  ", "   ", "   ", "** "))
+                          k = c("(15)", "(12)", "(114)", "(147)", "(141)", "(95)",
+                                "(33)", "(54)", "(47)", "(19)", "(116)",
+                                "(5)", "(37)", "(9)", "(48)", "(23)", "(123)", 
+                                "(17)", "(40)", "(87)", "(82)", "(42)", "(40)", 
+                                "(32)", "(32)", "(65)", "(66)", "(23)", "(24)",
+                                "(24)", "(23)", "(5)", "(5)", "(5)"),
+                          sig.level = c("***", "***", "***", "***", "***", "** ",
+                                        "***", "   ", "***", ".  ", "** ", 
+                                        "***", "** ", "*  ", "*  ", "** ", "***",
+                                        "** ", "*  ", "***", "***", ".  ", "***", 
+                                        "***", "***", "   ", "   ", "   ", "** ",
+                                        "   ", "   ", "** ", "   ", "   "))
+
 df_box_np_k$k_sig <- str_c(df_box_np_k$k, df_box_np_k$sig.level)
 
 df_box_np <- purrr::map_dfr(out_np, "df_box") |> 
@@ -318,13 +308,15 @@ full_df_trt_long <- full_df %>%
                             str_c(citation, "XXX", exp, "XXX", response, "XXX", "none"),
                             str_c(citation, "XXX", exp, "XXX", response, "XXX", species))) %>%
   dplyr::select(unique_id, fert, x_t, sd_t, rep_t) %>%
-  pivot_wider(names_from = fert, values_from = c(x_t, sd_t, rep_t)) %>%
+  pivot_wider(names_from = fert, values_from = c(x_t, sd_t, rep_t),
+              values_fn = mean) %>%
   full_join(full_df_control) %>%
   dplyr::select(unique_id,
                 x_c, sd_c, rep_c,
                 x_n = x_t_n, sd_n = sd_t_n, rep_n = rep_t_n,
                 x_p = x_t_p, sd_p = sd_t_p, rep_p = rep_t_p,
                 x_np = x_t_np, sd_np = sd_t_np, rep_np = rep_t_np)
+
 
 ###############################################################################
 # Calculate individual, main, and interaction effect sizes
@@ -376,18 +368,18 @@ CNP_effect_sizes <- data.frame(
 # `response` column
 ###############################################################################
 CNP_effect_sizes_reduced <- CNP_effect_sizes %>%
-  mutate(myvar = response,
-         myvar = ifelse(myvar == "amax", "asat", myvar),
-         myvar = ifelse(myvar == "anet", "asat", myvar),
-         myvar = ifelse(myvar == "fine_root_biomass" | myvar == "bnpp", 
-                        "bgb", myvar),
-         myvar = ifelse(myvar == "anpp", "agb", myvar),
-         myvar = ifelse(myvar == "total_leaf_area", "tla", myvar)) %>%
-  filter(myvar != "r_eco" & myvar != "nee" & 
-           myvar != "spad" & myvar != "tpu" &
-           myvar != "rd" & myvar != "stom_lim" &
-           myvar != "anet_mass" & myvar!= "lai" & myvar != "agb_np" &
-           myvar != "leaf_sugar_p" & myvar != "gpp")
+  filter(response %in% c("anpp_n", "anpp_p", "anpp", "leaf_n_mass", "leaf_p_mass", "anet",
+         "tbio_gm2", "bnpp", "rootshoot", "bgb", "lma",
+         "lai", "rmf", "tla", "gsw", "agb", "leaf_np", "total_biomass",
+         "asat", "leaf_wue", "leaf_n_area", "leaf_p_area", "vcmax", 
+         "jmax", "jmax_vcmax", "rd", "leaf_pnue", "leaf_ppue",
+         "leaf_structural_p", "leaf_metabolic_p", "leaf_nucleic_p",
+         "leaf_residual_p", "amax", "leaf_pi", "leaf_nre", "leaf_pre", 
+         "fine_root_biomass")) %>%
+  mutate(response = ifelse(response %in% c("amax", "anet", "asat"),
+                        "asat", response),
+         response = ifelse(response %in% c("bgb", "fine_root_biomass"),
+                        "bgb", response))
 
 write.csv(CNP_effect_sizes_reduced, "../data/CNPmeta_logr_results_int.csv", row.names = F)
 
@@ -399,24 +391,26 @@ write.csv(CNP_effect_sizes_reduced, "../data/CNPmeta_logr_results_int.csv", row.
 # Determine interaction effect size across multiple experiments. 
 # Takes into account experiment variance and uses experiment 
 # identity as grouping factor for random intercepts
-use_vars_int <- unique(CNP_effect_sizes_reduced$myvar)
+use_vars_int <- unique(CNP_effect_sizes_reduced$response)
 
 out_int <- purrr::map(as.list(use_vars_int),
                       ~analyse_meta_int(CNP_effect_sizes_reduced %>%
-                                          rename(var = myvar), nam_target = .))
+                                          rename(var = response), nam_target = .))
 names(out_int) <- use_vars_int
 
 df_box_int_k <- data.frame(var = use_vars_int,
-                          k = c("(18)", "(12)", "(125)", "(30)", "(23)", "(139)",
-                                "(133)", "(89)", "(42)", "(63)", "(40)", NA,
-                                "(37)", "(3)", "(43)", "(118)", "(88)", "(40)", 
-                                "(87)", "(82)", "(42)", "(40)", "(32)", "(61)", "(62)",
-                                "(20)", "(21)", "(20)", "(20)", "(5)"),
-                          sig.level = c("   ", "   ", "*  ", ".  ", "   ", "   ",
-                                        ".  ", "   ", "   ", "   ", "   ", NA,
-                                        "   ", "   ", ".  ", "** ", "   ", ".  ", 
-                                        "   ", "   ", "   ", "   ", "   ", "   ", "   ",
-                                        "   ", "   ", "   ", "*   ", "   "))
+                           k = c("(15)", "(12)", "(112)", "(147)", "(141)", "(95)",
+                                 "(33)", "(54)", "(47)", "(19)", "(116)",
+                                 "(5)", "(37)", "(9)", "(48)", "(23)", "(123)", 
+                                 "(17)", "(40)", "(87)", "(82)", "(42)", "(40)", 
+                                 "(32)", "(32)", "(65)", "(66)", "(23)", "(24)",
+                                 "(24)", "(23)", "(5)", "(5)", "(5)"),
+                           sig.level = c("   ", "   ", "** ", "   ", ".  ", "   ",
+                                         "   ", "   ", "   ", "   ", "   ",
+                                         "   ", "   ", "   ", "   ", "   ", "** ",
+                                         "   ", ".  ", "   ", "   ", "   ", "   ",
+                                         "   ", "   ", "   ", "   ", "   ", "   ",
+                                         "   ", ".  ", "   ", "   ", "   "))
 df_box_int_k$k_sig <- str_c(df_box_int_k$k, df_box_int_k$sig.level)
 
 
@@ -424,9 +418,9 @@ df_box_int <- purrr::map_dfr(out_int, "df_box") |>
   full_join(df_box_int_k) |>
   left_join(
     CNP_effect_sizes_reduced |> 
-      group_by(myvar) |> 
+      group_by(response) |> 
       summarise(intES_min = min(dNPi), intES_max = max(dNPi)) |> 
-      rename(var = myvar),
+      rename(var = response),
     by = "var")
 
 ##############################################################################
